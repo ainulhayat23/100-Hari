256135c9b14582c3d0a791ff008d240afcd7e4d3
ainulhayat23 <146690579+ainulhayat23@users.noreply.github.com>
2023-10-22 20:16:52 +0800

    Create 14_OperasiPembanding.java
    Operasi Pembanding bahasa pemrograman java

---
+++ 14_OperasiPembanding.java
@@ -0,0 +1,33 @@
+ package pkg_Harike14;
+public class Main{
+    
+    public static void main(String[] args) {
+        
+        // Hari ini kita akan mempelajari tentang 
+        // Operasi Pembanding
+        // == : Senilai
+        // != : tidak sama dengan
+        // >  : Lebih dari
+        // >= : lebih dari sama dengan
+        // <  : kurang dari
+        // <= : kurang dari sama dengan
+        // Dengan cara dibawah ini :
+        
+        
+        int nilaiA = 10;
+        int nilaiB = 10;
+        
+        System.out.println(nilaiA == nilaiB);
+        System.out.println(nilaiA != nilaiB);
+        System.out.println(nilaiA > nilaiB);
+        System.out.println(nilaiA >= nilaiB);
+        System.out.println(nilaiA < nilaiB);
+        System.out.println(nilaiA <= nilaiB);
+        
+        // Mungkin hanya ini yang bisa saya pelajari untuk hari ini
+        // jika ada kesalahan mohon dikoreksi 
+        // Terima kasih
+        
+    }
+    
+}
