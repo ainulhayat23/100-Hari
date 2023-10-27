5609bf768b38071de1b0e5b3d10f0b928cd746d7
ainulhayat23 <146690579+ainulhayat23@users.noreply.github.com>
2023-10-27 23:56:23 +0800

    Update and rename 19_PercabanganIF-ELSE.java to 19_PercabanganIf-Else.java
    Percabangan if else pada pemrograman java

--- 19_PercabanganIf-Else.java
+++ 19_PercabanganIf-Else.java
@@ -35,28 +35,28 @@ public class Main {
         
         
         Scanner masuk = new Scanner(System.in);
-        System.out.print("Masukkan total belanja : ");
+             System.out.print("Masukkan total belanja : ");
         
         double total = masuk.nextDouble();
-        System.out.print("Total belanja anda = Rp."+total);
+             System.out.print("Total belanja anda = Rp."+total);
         
         if(total >= 500000){
-        System.out.println("Dapat diskon 20% ");
-        double diskon = 20 * total / 100;
-        total -= diskon;
+          System.out.println("Dapat diskon 20% ");
+             double diskon = 20 * total / 100;
+                total -= diskon;
             
         } else if(total >= 250000){
-          System.out.println("Dapat diskon 15% ");
-          double diskon = 15 * total / 100;
-          total -= diskon;
+            System.out.println("Dapat diskon 15% ");
+              double diskon = 15 * total / 100;
+                total -= diskon;
             
         } else if(total >= 100000){
-          System.out.println("Dapat diskon 10% ");
-          double diskon = 10 * total / 100;
-          total -= diskon;
+            System.out.println("Dapat diskon 10% ");
+              double diskon = 10 * total / 100;
+                total -= diskon;
             
         } else {
-          System.out.println("Tidak dapat diskon");
+           System.out.println("Tidak dapat diskon");
             
         } System.out.println("Jadi total keseluruhan = Rp."+total);
         
