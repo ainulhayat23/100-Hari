c075e9436e6c0da48d131f5e91009c51e9136a8a
ainulhayat23 <146690579+ainulhayat23@users.noreply.github.com>
2023-11-02 17:36:40 +0800

    Create 25_SwitchCase.java
    Percabangan Switch Case

---
+++ 25_SwitchCase.java
@@ -0,0 +1,25 @@
+package pkg_Hari25;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        
+        // Switch case pada pemrograman java
+        
+        Scanner masuk = new Scanner(System.in);
+        System.out.print("Masukkan pilihan 1/2 : ");
+        int pil = masuk.nextInt();
+        switch(pil){
+            case 1:
+              System.out.println("Laki laki");
+              break;
+            
+            case 2:
+              System.out.println("Perempuan");
+              break;
+            
+            default:
+              System.out.println("Penyimpangan");
+              break;
+        }
+    }  
+}
