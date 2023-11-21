39b4f4173aba351deb9742de8dc7d5c39eb3f4a4
ainulhayat23 <146690579+ainulhayat23@users.noreply.github.com>
2023-11-21 20:59:31 +0800

    Create 44_MaksMinArray.java
    Mencari angka Maksimal dan Minimal menggunakan Array

---
+++ 44_MaksMinArray.java
@@ -0,0 +1,23 @@
+package pkg_Hari44;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        // Menentukan angka Maks dan Min menggunakan Array
+        
+      Integer [] a = {74,67,85,42,98,100,41,23,45,52,9,2,68};
+      int maks = Collections.max(Arrays.asList(a));
+      int min = Collections.min(Arrays.asList(a));
+      System.out.print("Deret angka : ");
+        
+        for(int i=0; i < a.length; i++){
+            System.out.print(a[i]+", ");
+            
+        }
+        System.out.println(""+"\n");
+        System.out.println("Angka Maksimal : "+maks);
+        System.out.print("Angka Minimal  : "+min);
+    }
+}
