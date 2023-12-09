adc772684f0746774ec649ffccea99c460c35ab9
ainulhayat23 <146690579+ainulhayat23@users.noreply.github.com>
2023-12-09 21:23:09 +0800

    Create 62_ubahArray.java
    Logika

---
+++ 62_ubahArray.java
@@ -0,0 +1,12 @@
+package pkg_hari62;
+import java.util.Arrays;
+public class Main {
+    
+    // Mengubah Array
+    
+    public static void main(String[] args) {
+        String [] Buah = {"Mangga","Jeruk","Pepaya"};
+        Buah [0] = "Durian";
+        System.out.println(Arrays.toString(Buah));
+    }
+}
