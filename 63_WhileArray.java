c507a210103299545646b3ddc676a62a0d41e704
ainulhayat23 <146690579+ainulhayat23@users.noreply.github.com>
2023-12-10 22:15:40 +0800

    Create 63_WhileArray.java
    Array menggunakan While

---
+++ 63_WhileArray.java
@@ -0,0 +1,13 @@
+package pkg_hari63;
+public class Main {
+    public static void main(String[] args) {
+        
+        double [] angka = {1.2 , 2.3 , 4.5 , 6.7 , 8,9};
+        System.out.println("While Array : \n");
+        int j = 0;
+        while(j < angka.length){
+            System.out.println("Index ke - "+ j + " = "+angka[j]);
+            j++;
+        }               
+    }
+}        
