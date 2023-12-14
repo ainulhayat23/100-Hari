8f3b1eaad3c2c9d1d466d58ade717dba615e986b
ainulhayat23 <146690579+ainulhayat23@users.noreply.github.com>
2023-12-14 21:44:29 +0800

    Create 67_Math.java
    Math pada java

---
+++ 67_Math.java
@@ -0,0 +1,14 @@
+package pkg_hari67;
+public class Main {
+    public static void main(String[] args) {
+        
+        // Math pada java
+        
+       int x = 87;
+       int y = 43;
+       int z = Math.min(x, y);
+       int v = Math.max(x, y);
+        System.out.println("Angka minimal  : "+z);
+          System.out.println("Angka Maksimal : "+v);
+    }
+}
