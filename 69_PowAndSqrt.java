38deca6fb8583fdd7b6aef43873b647b7f1070fb
ainulhayat23 <146690579+ainulhayat23@users.noreply.github.com>
2023-12-16 20:00:39 +0800

    Create 69_PowAndSqrt.java
    Akar dan Bilangan berpangkat

---
+++ 69_PowAndSqrt.java
@@ -0,0 +1,19 @@
+package pkg_hari69;
+public class Main {
+    public static void main(String[] args) {
+        
+        // Bilangan berpangkat
+      double x = 2;
+       double z = Math.pow(x,2);
+        System.out.print("Hasil dari 2 kuadrat adalah ");
+         System.out.println(z);
+        
+        System.out.println("=======================");
+        
+        // Menentukan Akar
+      double u = 100;
+       double i = Math.sqrt(u);
+        System.out.println("Hasil akar dari "+ u + " adalah ");
+         System.out.println(i);
+    }
+}
