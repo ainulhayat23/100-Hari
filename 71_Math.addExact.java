3ec770bdac9e25d92715f6db4f5aee18fce24b4a
ainulhayat23 <146690579+ainulhayat23@users.noreply.github.com>
2023-12-18 20:28:25 +0800

    Create 71_Math.addExact.java
    Penjumlahan math

---
+++ 71_Math.addExact.java
@@ -0,0 +1,16 @@
+package pkg_hari71;
+public class Main {
+    public static void main(String[] args) {
+        
+        // Math.addExact adalah metode dalam
+        // kelas Math pada bahasa pemrograman Java
+        // Yaitu penjumlahan dua variabel
+        
+     int nilaiA = 670;
+     int nilaiB = 432;
+      
+     int z = Math.addExact(nilaiA, nilaiB);
+        System.out.print("Hasil : ");
+           System.out.println(z);
+    }    
+}
