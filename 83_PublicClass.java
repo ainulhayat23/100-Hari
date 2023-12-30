ee6dbc1fb47764a6f57b1a9d47dd4388ab426a6f
ainulhayat23 <146690579+ainulhayat23@users.noreply.github.com>
2023-12-30 20:54:55 +0800

    Create 83_PublicClass.java

---
+++ 83_PublicClass.java
@@ -0,0 +1,14 @@
+package pkg_hari83;
+class Bahan {
+    int x = 10;
+    int y = 15;
+}
+
+public class Main {
+    public static void main(String[] args) {
+        
+       Bahan objek = new Bahan();
+        System.out.println(objek.x);
+        System.out.println(objek.y); 
+    }
+}
