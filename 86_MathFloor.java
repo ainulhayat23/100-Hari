19f22e1a26f3568c74a01771c40e92380b3c7865
ainulhayat23 <146690579+ainulhayat23@users.noreply.github.com>
2024-01-02 20:42:38 +0800

    Update 86_MathFloor.java

--- 86_MathFloor.java
+++ 86_MathFloor.java
@@ -3,15 +3,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         
-        Scanner scanner = new Scanner(System.in);
+        Scanner masuk = new Scanner(System.in);
 
         System.out.print("Masukkan angka desimal: ");
-        double angka = scanner.nextDouble();
+        double angka = masuk.nextDouble();
 
         double hasilFloor = Math.floor(angka);
 
         System.out.println("Hasil floor dari " + angka + " adalah: " + hasilFloor);
-
-        scanner.close();
     }
 }
