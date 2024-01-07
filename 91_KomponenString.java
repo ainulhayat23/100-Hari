73de3e7ef3402767577a285005d01c62bc546d29
ainulhayat23 <146690579+ainulhayat23@users.noreply.github.com>
2024-01-07 21:56:14 +0800

    Create 91_KomponenString.java

---
+++ 91_KomponenString.java
@@ -0,0 +1,13 @@
+package pkg_hari91;
+import java.lang.String;
+public class Main {
+    public static void main(String[] args) {
+        
+        String kalimat = "Saya suka bermain bola";
+        
+        System.out.println(kalimat.charAt(5));
+        
+        String kata = kalimat.substring(10,15);
+        System.out.print(kata);
+    }
+}
